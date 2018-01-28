e5e17b9dca6f4001c428b30b53a1a9c13e803429
Nagarjuna <nag@ng-js.com>
2018-01-28 19:04:30 +0530

    Added example for self invoked function

---
+++ b1/javascript/examples/self invoked function/math.js
@@ -0,0 +1,16 @@
+var math = {};
+
+function performAddition(a, b) {
+    return parseInt(a) + parseInt(b);
+}    
+
+function add(a, b) {
+    return performAddition(a, b);
+};
+
+function subtract(a, b) {
+    return a-b;
+};
+
+math.add = add;
+math.subtract = subtract;
