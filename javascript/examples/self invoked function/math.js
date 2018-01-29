0cb9feae98d26e216698430542fcaf2ac147623d
Nagarjuna <nag@ng-js.com>
2018-01-29 09:03:52 +0530

    added remove event listener example

--- javascript/examples/self invoked function/math.js
+++ javascript/examples/self invoked function/math.js
@@ -1,16 +1,20 @@
 var math = {};
 
-function performAddition(a, b) {
-    return parseInt(a) + parseInt(b);
-}    
 
-function add(a, b) {
-    return performAddition(a, b);
-};
+(function () {
+    function performAddition(a, b) {
+        return parseInt(a) + parseInt(b);
+    }    
+    
+    function add(a, b) {
+        return performAddition(a, b);
+    };
+    
+    function subtract(a, b) {
+        return a-b;
+    };
+    
+    math.add = add;
+    math.subtract = subtract;
+})()
 
-function subtract(a, b) {
-    return a-b;
-};
-
-math.add = add;
-math.subtract = subtract;
